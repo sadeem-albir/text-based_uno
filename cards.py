65f59b6583651af4be2234b5afb8a996f6208f3b
sadeem_albir <142955101+sadeem-albir@users.noreply.github.com>
2024-11-02 04:16:46 +0300

    Add files via upload

---
+++ cards.py
@@ -0,0 +1,185 @@
+import random
+
+class User:
+    def __init__(self, name, user_state, target_deck):
+        self.name = name
+        self.stack = [target_deck.pop() for _ in range(7)]
+        self.commands = ["put", "draw", "hand", "tb", "tb_full"]
+        # self.debug_commands = ["userhand", ""]
+        self.is_bot = user_state
+        self.troll_msg = [
+            "and apparently sucks at this game",
+            "and still apparently sucks at this game",
+            "and I'm wondering when they'll improve",
+            "ok I feel bad... sike, they still suck!"
+        ]
+        self.next_msg = 0
+
+    def format_card(self, card):
+        color_fmt = {
+            "r": "red",
+            "g": "green",
+            "b": "blue",
+            "y": "yellow",
+            "w": "wild",
+            "w4": "wild_draw4"
+        }
+        number_fmt = {
+            "r": "reverse",
+            "s": "skip",
+            "d": "draw2"
+        }
+
+        if card == "w" or card == "w4":
+            return color_fmt[card]
+        elif len(card) == 2 and card[0] in color_fmt and (card[1] in number_fmt or card[1].isdigit()):
+            return f"{color_fmt[card[0]]}_{number_fmt[card[1]] if not card[1].isdigit() else card[1]}"
+
+        return False
+
+    def put(self, card_i, tb):
+        if type(card_i) == int:
+            card_i -= 1
+            if not (0 <= card_i < len(self.stack)):
+                return (False, "invalid card index!")
+            card = self.stack[card_i]
+        else:
+            card_i = self.format_card(card_i) if self.format_card(card_i) else card_i
+            if card_i not in deck:
+                return (False, "invalid card name!")
+            elif card_i not in self.stack:
+                return (False, "you don't have that card!")
+            else:
+                card = card_i
+
+        card_loc = self.stack.index(card)
+        success = (True, "card added to table")
+        failure = (False, "incompatible card")
+
+        if not tb.card_is_compatible(self.stack[card_loc]):
+            return failure
+        else:
+            if "wild" in card:
+                if self.is_bot:
+                    user_color_choice = random.choice(["red", "green", "blue", "yellow"])
+                else:
+                    colors = {
+                        'r': "red",
+                        'g': "green",
+                        'b': "blue",
+                        'y': "yellow"
+                    }
+                    user_color_choice = colors[input("Enter color (r/g/b/y):").strip()]
+                tb.add(self.stack.pop(card_loc), color_choice=user_color_choice)
+                return success
+            else:
+                tb.add(self.stack.pop(card_loc))
+                return success
+
+
+    def draw(self, target_deck, tb):
+        if not target_deck:
+            tb.refill_deck(target_deck)
+            print("refilling the deck")
+            # print(f"\ndeck:\n{target_deck}")
+            # print(f"\ntable:\n{tb.stack}")
+
+        # print(f"stack before drawing card: {self.stack}")
+        drawn_card = target_deck.pop()
+        self.stack.append(drawn_card)
+
+        return drawn_card
+
+    def show_hand(self):
+        if len(self.stack) <= 10:
+            print(self.stack)
+        else:
+            print("[", end='')
+            for i in range(len(self.stack)):
+                print(f"{self.stack[i]}{', ' if i < len(self.stack)-1 else ']\n'}{'\n' if (i + 1) % 10 == 0 else ''}",
+                      end='')
+    def show_table(self, tb):
+        print(f"({len(tb.stack)}) [{tb.stack[-1]}]")
+
+    def show_full_table(self, tb):
+        if len(tb.stack) <= 10:
+            print(tb.stack)
+        else:
+            print("[", end='')
+            for i in range(len(tb.stack)):
+                print(f"{tb.stack[i]}{', ' if i < len(tb.stack)-1 else ']\n'}{'\n' if (i + 1) % 10 == 0 else ''}",
+                      end='')
+
+
+class Table:
+    first_card_filt = lambda self, card: "_" in card and card[card.index("_") + 1:].isdigit()
+    def __init__(self, target_deck):
+        self.first_card_i = next((i for i, card in enumerate(target_deck) if self.first_card_filt(card)), None)
+        self.stack = [target_deck.pop(self.first_card_i) if self.first_card_i is not None else None]
+        self.next_color = self.stack[-1][:self.stack[-1].index("_")] if self.stack[-1] is not None else None
+        self.next_number = self.stack[-1][self.stack[-1].index("_") + 1:] if self.stack[-1] is not None else None
+
+    def add(self, card, color_choice=None):
+        self.stack.append(card)
+        if color_choice is None:
+            if "_" not in card:
+                return (False, "error: undefined color choice for uncolored card")
+            self.next_color = self.stack[-1][:self.stack[-1].index("_")]
+            next_number = self.stack[-1][self.stack[-1].index("_") + 1:]
+            self.next_number = next_number
+            return (True, "card successfully added to table")
+        self.next_color = color_choice
+        self.next_number = None
+        return (True, "card successfully added to table")
+
+    def refill_deck(self, target_deck):
+        target_deck.extend(self.stack[:-1])
+        random.shuffle(target_deck)
+        self.stack = [self.stack[-1]]
+
+    def check_attack_state(self, users, turn, direction, target_deck):
+        card = self.stack[-1]
+        if "_" not in card:
+            return (False, "no action card", turn, direction)
+        
+        card_action = card[card.index("_") + 1:]
+        if card_action.isdigit():
+            return (False, "no action card", turn, direction)
+
+        if card_action in ["draw4", "draw2"]:
+            # nx: next user
+            nx = (turn + direction) % len(users)
+            num_draws = int(card_action[-1])
+            for _ in range(num_draws):
+                users[nx].draw(target_deck, self)
+            msg = f"{users[nx].name} draws {num_draws}"
+            if not users[nx].is_bot:
+                msg += f" ({users[nx].troll_msg[users[nx].next_msg] if users[nx].next_msg < len(users[nx].troll_msg) else ''})"
+                users[nx].next_msg = users[nx].next_msg + 1 if users[nx].next_msg < len(users[nx].troll_msg) else users[nx].next_msg
+            return (True, msg, turn + direction, direction)
+        elif card_action == "skip":
+            return (True, f"{users[(turn + direction) % len(users)].name} is skipped", turn + direction, direction)
+        elif card_action == "reverse":
+            return (True, "direction is reversed", turn, -direction)
+
+    def card_is_compatible(self, card):
+        if "wild" in card:
+            return True
+        if "_" not in card:
+            return False
+        elif card[:card.index("_")] == self.next_color or card[card.index("_") + 1:] == self.next_number:
+            return True
+        return False
+
+deck = []
+
+for i in "red green blue yellow".split():
+    deck.extend([f"{i}_{j}" for j in range(10)])
+    deck.extend([f"{i}_{j}" for j in range(1, 10)])
+    for _ in range(2):
+        deck.extend([f"{i}_{j}" for j in "skip reverse draw2".split()])
+    deck.extend(["wild", "wild_draw4"])
+
+applied_deck = [card for card in deck]
+random.shuffle(applied_deck)
+
---
+++ main.py
@@ -0,0 +1,127 @@
+import cards
+import random
+import time
+
+def main():
+    try:
+        while not (username := input("Enter your name: ").strip()):
+            continue
+        names = [username, "bot1", "bot2", "bot3"]
+    except (EOFError, KeyboardInterrupt):
+        return
+
+    states = [False, True, True, True]
+    users = [cards.User(name, state, cards.applied_deck) for name, state in zip(names, states)]
+    table = cards.Table(cards.applied_deck)
+
+    turn, direction = 0, 1
+    game_status, winning_user_index = check_game_status(users)
+
+    print(f"Uno Game! Commands: {users[0].commands}")
+    while not game_status:
+        try:
+            game_state = next_turn(users, table, turn, direction, cards.applied_deck)
+        except (EOFError, KeyboardInterrupt):
+            return
+
+        card_attack, message, turn, direction = game_state
+
+        turn = (turn + direction) % len(users)
+        if card_attack:
+            print(message)
+
+        game_status, winning_user_index = check_game_status(users)
+
+
+    print(f"Good game! {users[winning_user_index].name} won, congratulations!")
+
+def check_game_status(users):
+    for i, user in enumerate(users):
+        if len(user.stack) == 0:
+            return True, i
+    return False, None
+
+def next_turn(users, tb, turn, direction, target_deck):
+    if users[turn].is_bot:
+        bot_turn(users[turn], tb, target_deck)
+    else:
+        user_turn(users[turn], tb, target_deck)
+
+    return tb.check_attack_state(users, turn, direction, target_deck)
+
+
+def user_turn(user, tb, target_deck):
+    print(f"\nyour hand: {user.stack}")
+    print(f"({len(tb.stack)}) table: [{tb.stack[-1]}]")
+    print()
+    while True:
+        commands = input(f"({len(user.stack)}) {user.name}'s turn: ").split()
+        if len(commands) == 0:
+            continue
+
+        if commands[0] not in user.commands:
+            print("invalid command")
+            continue
+        if commands[0] == "put":
+            if len(commands) == 1:
+                card = input("Enter card: ").strip()
+                card = int(card) if card.isdigit() else card
+                put_status = user.put(card, tb)
+                print(put_status[1])
+                if not put_status[0]:
+                    continue
+                else:
+                    print(f"{user.name} -> {tb.stack[-1]}")
+                    break
+            elif len(commands) == 2:
+                if commands[1].isdigit():
+                    put_status = user.put(int(commands[1]), tb)
+                else:
+                    put_status = user.put(commands[1], tb)
+                if not put_status[0]:
+                    print(put_status[1])
+                    continue
+                else:
+                    print(f"{user.name} -> {tb.stack[-1]}")
+                    break
+        elif commands[0] == "draw":
+            user.draw(target_deck, tb)
+            print(f"{user.name} <- {user.stack[-1]}")
+        elif commands[0] == "hand":
+            user.show_hand()
+        elif commands[0] == "tb":
+            user.show_table(tb)
+        elif commands[0] == "tb_full":
+            user.show_full_table(tb)
+
+
+def bot_turn(bot, tb, target_deck):
+    print()
+    print(f"({len(bot.stack)}) {bot.name}'s turn...")
+    # print(f"{bot.name}'s stack: {bot.stack}")
+    time.sleep(random.randint(1, 3))
+
+    compatible_cards = []
+    for card in bot.stack:
+        if tb.card_is_compatible(card):
+            compatible_cards.append(card)
+    
+    if not compatible_cards:
+        drawn_card = ""
+        while not tb.card_is_compatible(drawn_card):
+            drawn_card = bot.draw(target_deck, tb)
+            print(f"{bot.name} <- {drawn_card}")
+        compatible_cards.append(drawn_card)
+    while True:
+        bot_card = random.choice(compatible_cards)
+        if "wild" in bot_card or bot.put(bot_card, tb)[0]:
+            bot.put(bot_card, tb)
+            print(f"{bot.name} -> {bot_card}")
+            # print(f"{bot.name}'s stack: {bot.stack}")
+            break
+#        elif bot.put(bot_card, tb)[0]:
+#            break
+
+
+if __name__ == '__main__':
+    main()
